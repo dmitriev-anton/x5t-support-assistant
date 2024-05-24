15e6fd367aa9cc389115f7852433846d6237e175
Anton Dmitriev <spaceboobman@gmail.com>
2024-05-24 13:01:36 +0400

    2.16 version set monospace font to output for pretty tables look, improved reports for better look

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ g_list = group_list()
 
 def main_window():
     """App gui"""
-    # SG.set_options(font=("DejaVu Sans Mono", 10))
+    SG.set_options(font=("Segoe UI", 11))
     SG.theme('DarkGreen5')
 
     vehicle_tab_layout = [
