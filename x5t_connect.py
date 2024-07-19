5741cc1a0f1c5ceab36f4e8346fe357cfef3908d
Anton Dmitriev <spaceboobman@gmail.com>
2024-07-19 10:35:53 +0400

    Moved to the PySimleGUI-4-foss

--- x5t_connect.py
+++ x5t_connect.py
@@ -59,19 +59,19 @@ def str_dict(dict) -> str:
     return result
 
 
-_SQL = """select "number" , "name" , phone, licence, auth_user_id, 
-    id, status, "type", driver_id, ut, birth_date, request_number, block_date
-    from "core-drivers-schema".drivers where number = '{0}'"""
+# _SQL = """select "number" , "name" , phone, licence, auth_user_id,
+#     id, status, "type", driver_id, ut, birth_date, request_number, block_date
+#     from "core-drivers-schema".drivers where number = '{0}'"""
 
-_SQL2 = """ select t1.phone,t2.waybillid as PL,t2.status,t2.driver_status,t4.sap_number,t2."version", t2.driver_version,t4.plan_start_date, t4.tms_number,t4.id,t3.is_mfp, t5.status 
-    from "core-drivers-schema".drivers t1
-    inner join "core-waybills-schema".waybills t3 on t3.driver_number=t1."number" and t3.user_status = 'E0002' and t3.system_status = 'I0070'
-    inner join "core-invoices-schema".own_trip t2 on t3."number"=t2.waybillid 
-    inner join "core-drivers-schema".driver_status t5 on t5.waybill_id = t2.waybillid 
-    inner join "core-invoices-schema".invoice t4 on t4.id=t2.invoice_id
-    where t1."number" =  '{0}'"""
+# _SQL2 = """ select t1.phone,t2.waybillid as PL,t2.status,t2.driver_status,t4.sap_number,t2."version", t2.driver_version,t4.plan_start_date, t4.tms_number,t4.id,t3.is_mfp, t5.status
+#     from "core-drivers-schema".drivers t1
+#     inner join "core-waybills-schema".waybills t3 on t3.driver_number=t1."number" and t3.user_status = 'E0002' and t3.system_status = 'I0070'
+#     inner join "core-invoices-schema".own_trip t2 on t3."number"=t2.waybillid
+#     inner join "core-drivers-schema".driver_status t5 on t5.waybill_id = t2.waybillid
+#     inner join "core-invoices-schema".invoice t4 on t4.id=t2.invoice_id
+#     where t1."number" =  '{0}'"""
 
-_SQL3 = """SELECT df.feature_id,dfd."name", dfd.description  FROM "core-drivers-schema".driver_features df 
-    inner join "core-drivers-schema".drivers dr on df.driver_id = dr.id
-    inner join "core-drivers-schema".driver_feature_dictionary dfd on dfd.id = df.feature_id 
-    where dr.number = '{0}'"""
+# _SQL3 = """SELECT df.feature_id,dfd."name", dfd.description  FROM "core-drivers-schema".driver_features df
+#     inner join "core-drivers-schema".drivers dr on df.driver_id = dr.id
+#     inner join "core-drivers-schema".driver_feature_dictionary dfd on dfd.id = df.feature_id
+#     where dr.number = '{0}'"""
