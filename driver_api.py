ef8144c45ec967a503dc95dbe5bb8389c76d3e53
Anton Dmitriev <spaceboobman@gmail.com>
2025-04-01 16:59:28 +0400

    adapting moving x5t to salt 4

--- driver_api.py
+++ driver_api.py
@@ -12,44 +12,49 @@ if getattr(sys, 'frozen', False):
 load_dotenv(dotenv_path=os.path.join(extDataDir, '.env'))
 reg_api = os.getenv("DRIVER_REG_API")
 info_api = os.getenv("DRIVER_INFO_API")
+app_fleet  = os.getenv("APP_FLEET")
 
 
 def driver_pwd_reset(phone: str, password: str) -> object:
     """Сброс пароля"""
 
     def api_pwd_recovery_request(phone: str):
-        api = f'http://{reg_api}/api/v1/driver/password/recovery'
+        api = f'http://{app_fleet}/info/v1/driver/password/recovery'
 
         headers = {
             'Content-Type': 'application/json',
             'Content-Length': '68',
-            'Host': reg_api,
+            'Host': app_fleet,
+            'User-Agent': 'X5 Transport NEW/versionName=25.3.20 versionCode=2503020',
         }
         body = {
-            'data': {'phone': phone},
-            'params': 'null'
+            'data': {'phone': phone },
         }
 
         try:
             response = requests.post(api, headers=headers, data=json.dumps(body), timeout=30, verify=False)
+            # print(response)
             return response.json()['data']['verificationSessionId']
         except requests.exceptions.SSLError:
             return None
+
 
     def pwd_code(verification_id: str):
         _ = ('SELECT code FROM \"core-verification-schema\".phone_verification_session '
              'where status = \'ACTIVE\' and id = \'{0}\'')
         temp = None
         temp = db_request(_.format(verification_id))
+        print(temp)
         return temp[0]['code']
 
     def api_pwd_verify(code, phone, verification_id: str):
 
-        api = f'http://{reg_api}/api/v1/driver/sms/verify'
+        api = f'http://{app_fleet}/info/v1/driver/sms/verify'
         headers = {
             'Content-Type': 'application/json',
             'Content-Length': '129',
-            'Host': reg_api,
+            'Host': app_fleet,
+            'User-Agent': 'X5 Transport NEW/versionName=25.3.20 versionCode=2503020',
         }
         body = {
             'data': {
@@ -62,28 +67,32 @@ def driver_pwd_reset(phone: str, password: str) -> object:
 
         try:
             response = requests.post(api, headers=headers, data=json.dumps(body), timeout=30, verify=False)
+            # print(response.json())
             return response.json()['data']['result']
         except requests.exceptions.SSLError:
             return None
 
-    def api_pwd_create(password, verification_id: str):
+    def api_pwd_create(phone, password, verification_id: str):
 
-        api = f'http://{reg_api}/api/v1/driver/password/create'
+        api = f'http://{app_fleet}/info/v1/driver/password/create'
         headers = {
             'Content-Type': 'application/json',
             'Content-Length': '122',
-            'Host': reg_api,
+            'Host': app_fleet,
+            'User-Agent': 'X5 Transport NEW/versionName=25.3.20 versionCode=2503020',
         }
         body = {
             'data': {
+                # 'phone' : phone,
                 'password': password,
                 'verificationSessionId': verification_id
             },
-            'params': 'null'
         }
         try:
             response = requests.post(api, headers=headers, data=json.dumps(body), timeout=30, verify=False)
-            return response.json()['data']['result']
+            # print(response.json())
+            return response.json()
+
         except requests.exceptions.SSLError:
             return None
 
@@ -92,7 +101,7 @@ def driver_pwd_reset(phone: str, password: str) -> object:
         pass_code = pwd_code(verification_id)
         if api_pwd_verify(pass_code, phone, verification_id):
             # password = ''.join(str(random.randint(0, 9)) for _ in range(6))
-            result = api_pwd_create(password=password, verification_id=verification_id)
+            result = api_pwd_create(phone=phone, password=password, verification_id=verification_id)
             if result:
                 return True
             else:
@@ -110,7 +119,7 @@ def api_driver_token(phone: str) -> str:
     headers = {
         'Content-Type': 'application/json',
         'Cookie': '17b4e09f4ad0242ff0dcd2969ae02791=5dc45d0378d1e3bd72bbffecd742833e',
-        'User-Agent': 'X5 Transport NEW/versionName=24.7.15 versionCode=2407015',
+        'User-Agent': 'X5 Transport NEW/versionName=24.3.20 versionCode=2503020',
         #'Host': info_api,
     }
     body = {
@@ -128,4 +137,4 @@ def api_driver_token(phone: str) -> str:
     except requests.exceptions.SSLError:
         raise ConnectionError('Ошибка связи.')
 
-#print(api_driver_token('9898570346'))
+# print(driver_pwd_reset('9110916360', '111112'))
